b6b279d166f42faad9274440b8e5e507c5f584f2
Danny White <hi@dannywhite.org>
2022-08-20 20:46:09 +1000

    exp: Netlify function

---
+++ netlify/functions/rebuild.js
@@ -0,0 +1,20 @@
+// https://docs.netlify.com/netlify-labs/experimental-features/scheduled-functions/
+// https://www.raymondcamden.com/2022/02/04/an-early-look-at-netlify-scheduled-functions
+
+const { schedule } = require('@netlify/functions')
+const fetch = require('node-fetch')
+
+const REBUILD_URL = process.env.rebuildurl
+
+const handler = async function (event, context) {
+    console.log("Received event:", event)
+
+    await fetch(REBUILD_URL, { method: 'POST' })
+
+    return {
+        statusCode: 200,
+    };
+};
+
+// Match the cron expression from Ephemerabot
+module.exports.handler = schedule("0 8 * * *", handler);
