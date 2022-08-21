6047bf407c76db03f45af7eb038decf1416a85e9
Danny White <hi@dannywhite.org>
2022-08-21 18:22:54 +1000

    fix: cloudinary link

--- src/helpers/shortcodes.js
+++ src/helpers/shortcodes.js
@@ -5,9 +5,9 @@
 // "The sizes attribute can be tricky to write by hand..."
 
 // Set constants for the Cloudinary URL and fallback widths for images when not supplied by the shortcode params
-const CLOUDNAME = "dannywhite"
-// Assume we're looking directly in the 'notes' folder
-const FOLDER = "ephemera/"
+const CLOUDNAME = "ephemera"
+// Files are in root so leave blank
+const FOLDER = ""
 const BASE_URL = `https://res.cloudinary.com/${CLOUDNAME}/image/upload/`;
 // TODO: Why is each photo going with 1360 despite me passing in a 'width' attribute?
 const FALLBACK_WIDTHS = [320, 640];
